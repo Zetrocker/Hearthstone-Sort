3a4b45d5b4e56887cc431dfb4c62306d289fd880
Michael Cortez <michaelcortezster@gmail.com>
2014-10-30 01:38:50 -0400

    cleaned up compare, rate

--- compare.py
+++ compare.py
@@ -20,6 +20,7 @@ def update(file, url):
 if os.path.isfile(file) is False:
     update(file, url)
 
+
 def update_prompt():
     """
     asks for update if data is older than a day
@@ -33,134 +34,129 @@ def update_prompt():
         update_ask = input('Download update file from LiquidHearth? Y/N ')
         update_ask.lower()
         if update_ask == 'y':
-            download = requests.get(url).text
-            open(file, 'r+b').write(bytes(download, 'UTF-8'))
-            print("Downloaded update from: ", url, '\n')
+            update(file, url)
+            # download = requests.get(url).text
+            # open(file, 'r+b').write(bytes(download, 'UTF-8'))
+            # print("Downloaded update from: ", url, '\n')
         else:
             print("Skipping Download\n")
 
 update_prompt()
-# print("last modified: %s" % time.ctime(os.path.getmtime(file)))
-# print("created: %s" % time.ctime(os.path.getctime(file)))
-# up_x = up_x.lower()
-# if up_x == 'y':
-#
-# file = "data.csv"
-# download = requests.get(url).text
-#     open(file, 'r+b').write(bytes(download, 'UTF-8'))
-#
-# if up_x == 'n':
-#     print('Skipping download\n')
-# else:
-#     print('invalid selection\n')
 
-# def ask_Clean(card1, card2):
-#     """
-#
-#     :param card1:
-#     :param card2:
-#     :return:
-#     """
-#     global card1
-#     global card2
-#     card = {c}
 
-card1 = input('First Card: ')
-# card1 = "Cairne Bloodhoof"
-card2 = input('Second Card: ')
-# card2 = 'Ysera'
 
-# http://graphemica.com/%E2%B1%A1
-# Need to replace *, -, and latin small l with double bar with ""
+
+
+
+
+
+card1 = "Cairne Bloodhoof"
+# card1 = input('First Card: ')
+
+card2 = 'Ysera'
+# card2 = input('Second Card: ')
+
+
+
+
+def regex(str):
+    """
+     removes special characters
+     :type x: str
+     :return z: str
+     """
+    garbage = True if re.search('[0-9A-Za-z]', str) else False
+    if garbage:
+        fixed = ''.join([word.group() for word in re.finditer(r'[0-9A-Za-z\s]+', str)])
+        return fixed
+
+
+
+
 card_dict = dict(
     [('Best', []), ('Excellent', []), ('Good', []), ('Average', []), ('Poor', []), ('Terrible', [])])
 # card_dict = dict(lambda x: x for class_ranking[range(1, 7)])
+
 class_ranking = ['rank', 'Best', 'Excellent', 'Good', 'Average', 'Poor', 'Terrible']
+
 with open('data.csv', 'r') as csvfile:
     data = csv.reader(csvfile)
     data.__next__()
     for x in data:
         for y in range(1, 7):
+            card = regex(x[y])
             quality = class_ranking[y]
-            card_dict[quality].append(x[y])
-
-# for i in reversed(class_ranking):
-#     rank_quality = i
-#     print(i)
+            card_dict[quality].append(card)
 
 
 def rate(c):
     """
     this will find the given cards, and return a quality for the best and so forth
-    :rtype : int, False if no other return
+    :rtype : list [Card, int for class, row number
     """
     rank = class_ranking[::-1]
-    global card_quality
-    global quality
-    for quality, cards in card_dict:
-        # for cards in card_dict[quality]:
-        if c.lower() == cards.lower():
-            card_quality = rank.index(quality)
-            return card_quality
-    return False
-
+    for qualities in card_dict:
+        for cards in card_dict[qualities]:
+            if cards is not None and c.lower() == cards.lower():
+                trump_rank = rank.index(qualities)
+                row_rank = card_dict[qualities].index(cards)
+                card_tuple = (cards, trump_rank, row_rank)
+                card_rating = list(card_tuple)
+                return card_rating
+    return print("could not find card", c)
 
 
 def compare(a, b):
     """
     this will rate the cards input by best excellent.. if there is a tie it will pass the cards to a tiebreaker function
+    :rtype : string describing winner
     :param a:
     :param b:
     :return:
     """
-
-    if rate(a) and rate(b) is not False:
-        c1_quality = rate(a)
-        c2_quality = rate(b)
-    if c1_quality > c2_quality:
-        print(a, "is better than", b)
-    elif c2_quality > c1_quality:
-        print(b, "is better than", a)
+    c1_quality = rate(a)
+    c2_quality = rate(b)
+    if type(c1_quality) is list and type(c2_quality) is list:
+        card1wins = c1_quality[0], "is better than", c2_quality[0]
+        card2wins = c2_quality[0], "is better than", c1_quality[0]
+        if c1_quality[1] > c2_quality[1]:
+            print(card1wins)
+        elif c1_quality[1] < c2_quality[1]:
+            print(card2wins)
+        elif c1_quality[1] == c2_quality[1]:
+            #tiebreaker
+            if c1_quality[2] < c2_quality[2]:
+                print(card1wins)
+            elif c1_quality[2] > c2_quality[2]:
+                print(card2wins)
+            elif c1_quality[2] == c2_quality[2]:
+                print("Those are the same card sillypants!")
     else:
-        tiebreaker(a, b)
+        print("I can't believe you've done this!")
 
 
-def tiebreaker(a, b):
-    global breaker1
-    global breaker2
-    # breaker1, breaker2 = int, int
-    for quality in card_dict:
-        for card in card_dict[quality]:
-            if a.lower() == card.lower():
-                breaker1 = card_dict[quality].index(card)
-            if b.lower() == card.lower():
-                breaker2 = card_dict[quality].index(card)
-    if breaker1 == breaker2:
-        print("Those are the same card sillypants!")
-    if breaker1 < breaker2:
-        print(a, "is better than", b)
-    if breaker2 < breaker1:
-        print(b, "is better than", a)
-
-#TODO: make regular expression for this, and refactor this for regex
-def homogenize(a):
-    return a.lower()
 
 
-# def purge(x):
-#     """
-#     removes special characters
-#     :type x: str
-#     :return z: str
-#     """
-#     global fixed
-#     y = True if re.search('[0-9A-Za-z]', x) else False
-#     if y:
-#         fixed = ''.join([word.group() for word in re.finditer(r'[0-9A-Za-z\s]+', x)])
-#     return fixed
+
+
+# def tiebreaker(a, b):
+#     global breaker1
+#     global breaker2
+#     # breaker1, breaker2 = int, int
+#     for quality in card_dict:
+#         for card in card_dict[quality]:
+#             if a.lower() == card.lower():
+#                 breaker1 = card_dict[quality].index(card)
+#             if b.lower() == card.lower():
+#                 breaker2 = card_dict[quality].index(card)
+#     if breaker1 == breaker2:
+#         print("Those are the same card sillypants!")
+#     if breaker1 < breaker2:
+#         print(a, "is better than", b)
+#     if breaker2 < breaker1:
+#         print(b, "is better than", a)
+
 
 compare(card1, card2)
 
-# True if re.search('[0-9A-Fa-f]', 'g') else False
 
-# print(''.join([word.group() for word in re.finditer(r'[0-9A-Za-z\s]+', '!@#HI DaVE!@#!@# THERE')]))
